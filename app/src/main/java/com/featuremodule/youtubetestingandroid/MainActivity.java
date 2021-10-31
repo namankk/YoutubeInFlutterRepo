b003aca7ad650dd350a805ecba34f70e803fb1e2
naman.k <naman.k@purplle.com>
2021-10-31 10:13:53 +0530

    - Android module first commet

---
+++ app/src/main/java/com/featuremodule/youtubetestingandroid/MainActivity.java
@@ -0,0 +1,62 @@
+package com.featuremodule.youtubetestingandroid;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import io.flutter.embedding.android.FlutterView;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends AppCompatActivity {
+    FlutterView flutterView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        flutterView = findViewById(R.id.flutter_view_main);
+        setSupportActionBar(toolbar);
+        FlutterEngine flutterEngine = new FlutterEngine(getApplicationContext());
+        flutterEngine.getNavigationChannel().setInitialRoute("/");
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault()
+        );
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        flutterView.attachToFlutterEngine(flutterEngine);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
